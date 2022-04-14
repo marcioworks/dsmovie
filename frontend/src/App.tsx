551dcf63ccca85888aab1d2a316beae46b2729b1
Marcio Santos <marcioadsworks@gmail.com>
2022-04-14 11:10:54 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Página principal</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
